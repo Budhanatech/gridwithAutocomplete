947fe66fb19e0c82b0c317b206a72d202505fce0
jay <budhanatech@gmail.com>
2020-03-22 21:08:59 +0530

    fix datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MaterialModule } from './material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
